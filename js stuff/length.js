4d88ba8a3c9ba9188adeda3771910d949ea1eb33
lakshya raghuwanshi <168985074+lakshzero@users.noreply.github.com>
2025-04-29 20:04:08 +0530

    Add files via upload

---
+++ js stuff/length.js
@@ -0,0 +1,3 @@
+var tweet = prompt("write your tweet");
+var count = tweet.length;
+console.log("You have written " + count + " chracters,you have "+ (140-count)+" characters remaning");
---
+++ js stuff/stringconcatenation.js
@@ -0,0 +1,4 @@
+var message = "hello";
+var name = "laksh";
+
+console.log(message + " there, " + name);
