9759cb5a617a6a394622e16d8a04531c4eb63cb0
lakshya raghuwanshi <168985074+lakshzero@users.noreply.github.com>
2025-05-03 23:10:06 +0530

    Add files via upload

---
+++ js stuff/arugmentparameter.js
@@ -0,0 +1,23 @@
+function getMilk(money) {   
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+    var bottels = Math.floor(money/1.5); //or just use parseint 
+    console.log("Buy "+bottels+" bottels of milk");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+  }
+  getMilk(10);
