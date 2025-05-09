938e378f39fa896cfe57271db5fe47645fe28c0b
lakshya raghuwanshi <168985074+lakshzero@users.noreply.github.com>
2025-05-09 22:31:05 +0530

    Add files via upload

---
+++ js stuff/fibonnacci.js
@@ -0,0 +1,16 @@
+function fibonacciGenerator(n) {
+    var arr = [];
+    for (var i = 0; i < n; i++) {
+        if (i === 0) {
+            arr.push(0);
+        } else if (i === 1) {
+            arr.push(1);
+        } else {
+            arr.push(arr[i - 1] + arr[i - 2]);
+        }
+    }
+    return arr;
+}
+
+console.log(fibonacciGenerator(20));
+
