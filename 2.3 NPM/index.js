7b57a0a2636644e9069a6e70f5fc1c817ae13936
lakshya raghuwanshi <168985074+lakshzero@users.noreply.github.com>
2025-05-24 22:31:09 +0530

    Add files via upload

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,11 @@
+//var generateName = require ("sillyname"); // var require module silly name
+//sillyname
+import   generateName  from  "sillyname"
+var sillyname = generateName();
+
+console.log (`My name is ${sillyname}`);
+//super hero name
+import {randomSuperhero} from "superheroes";
+const name = randomSuperhero();
+
+console.log (`I am ${name}`);
